3155f0a6525db042f3eceeba0437c3753c4c5397
Christian Thiel <christian.thiel@gec.io>
2022-11-04 08:22:08 +0100

    add output-options

--- service/src/main/java/org/openapitools/openapidiff/sample/boundary/DeltaResource.java
+++ service/src/main/java/org/openapitools/openapidiff/sample/boundary/DeltaResource.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -18,12 +19,15 @@ import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 import org.jboss.resteasy.annotations.providers.multipart.MultipartForm;
 import org.openapitools.openapidiff.core.OpenApiCompare;
 import org.openapitools.openapidiff.core.model.ChangedOpenApi;
+import org.openapitools.openapidiff.core.output.JsonRender;
 import org.openapitools.openapidiff.core.output.Markdown2HtmlRender;
+import org.openapitools.openapidiff.core.output.MarkdownRender;
+import org.openapitools.openapidiff.core.output.YamlRender;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @RequestScoped
-@Path("/resources/delta/html")
+@Path("/resources/delta")
 public class DeltaResource {
 
     private static final Logger log = LoggerFactory.getLogger(DeltaResource.class);
@@ -50,8 +54,13 @@ public class DeltaResource {
                                 "on".equals(aInput.ignoreDescriptions),
                                 "on".equals(aInput.ignoreResponseHeader),
                                 aInput.filterPath));
-        String html = new Markdown2HtmlRender().render(result);
-        return Response.ok(html, MediaType.TEXT_HTML).build();
+        return switch(aInput.format) {
+            case "html" -> Response.ok(new Markdown2HtmlRender().render(result), MediaType.TEXT_HTML).build();
+            case "markdown" -> Response.ok(new MarkdownRender().render(result), MediaType.TEXT_PLAIN).build();
+            case "yaml" -> Response.ok(new YamlRender().render(result), MediaType.TEXT_PLAIN).build();
+            case "json" -> Response.ok(new JsonRender().render(result), MediaType.APPLICATION_JSON).build();
+            default -> Response.ok(new Markdown2HtmlRender().render(result), MediaType.TEXT_HTML).build();
+        };
     }
 
     public static class DeltaInput {
@@ -79,5 +88,23 @@ public class DeltaResource {
         @Schema(description = "ignore all security-infos in delta-evaluation", enumeration = {"on", "off"}, required = true, defaultValue = "on")
         @FormParam("ignoreSecurity")
         String ignoreSecurity;
+
+        @Schema(description = "output format", enumeration = {"html", "markdown", "json", "yaml"}, required = true, defaultValue = "html")
+        @FormParam("format")
+        @DefaultValue("html")
+        String format;
+
+        @Override
+        public String toString() {
+            return "DeltaInput{" 
+                    + "oldFile=" + oldFile + ", "
+                    + "newFile=" + newFile + ", "
+                    + "filterPath=" + filterPath + ", "
+                    + "ignoreDescriptions=" + ignoreDescriptions + ", "
+                    + "ignoreResponseHeader=" + ignoreResponseHeader + ", "
+                    + "ignoreSecurity=" + ignoreSecurity + ", "
+                    + "format=" + format 
+                    + '}';
+        }
     }
 }
