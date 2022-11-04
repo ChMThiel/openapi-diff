31e25a9d641fb9b2795abdc6fce851605fcbb256
Christian Thiel <christian.thiel@gec.io>
2022-11-04 11:26:12 +0100

    moved service to separate project

--- service/git-tag-and-push-next-version.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-NEXT_VERSION=$(cat .newversion)
-git tag ${NEXT_VERSION}
-git push --tags
--- service/static-code-analysis.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DUPLICATE_SWAGGER_OPERATION_IDS=$(grep -E "@Operation" -r . | grep -o -E "operationId.?=.?\"[A-Za-z\.]*\"" | cut -d "=" -f 2 | tr -d '"' | sort | uniq -c | sort | awk '$1 >= 2')
-
-if [[ -n "$DUPLICATE_SWAGGER_OPERATION_IDS" ]]
-then
-  echo "Duplicate Swagger operationIds found, aborting: $DUPLICATE_SWAGGER_OPERATION_IDS"
-  exit 1
-fi
